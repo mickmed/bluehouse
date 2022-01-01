10bba6635c27c7fec05f34758eb5d6ab2abb0d49
mick <mick2090@gmail.com>
2022-01-01 14:19:00 -0500

    user login

--- src/App.js
+++ src/App.js
@@ -4,25 +4,28 @@ import Home from './Components/Home.jsx'
 import Layout from './Components/Layout.jsx'
 import './App.scss';
 
-
-
-
 function App() {
-  return (
-    <div className="App">
-      <Header className="App-header" />
-      
-      <main>
-        <Layout>
-          <Route exact path='/'>
-            <Home/>
-          </Route>
-        </Layout>
-
-
-      </main>
-    </div>
-  );
+  let username = prompt('username')
+  let pass
+  if (username === process.env.REACT_APP_USERNAME) {
+    pass = prompt('password')
+    if (pass === process.env.REACT_APP_USERPW) {
+      return (
+        <div className="App">
+          <Header className="App-header" />
+          <main>
+            <Layout>
+              <Route exact path='/'>
+                <Home />
+              </Route>
+            </Layout>
+          </main>
+        </div>
+      );
+    }
+  } else {
+    return <div>your login failed</div>
+  }
 }
 
 export default App;
